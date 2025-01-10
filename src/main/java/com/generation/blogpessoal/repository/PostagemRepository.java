8ae779ba496520b6b9006b7dfc8d8700cee1e041
Wandsonslopes <wandson_lopes@hotmail.com>
2025-01-10 16:54:52 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,21 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	// save(Objeto objeto)
+	
+	// findById(Long id)
+	
+	// existById(Long id)
+	
+	// findAll()
+	
+	// deleteById(Long id)
+	
+	// deleteAll()
+
+}
