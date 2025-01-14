e6a4b735d057d73d9e2e81c256e16a266ae88c7e
Wandsonslopes <wandson_lopes@hotmail.com>
2025-01-14 20:09:02 -0300

    Incluindo Recurso Tema

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,59 +4,78 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity                       // indicar que a classe é uma tabela
+@Entity // indicar que a classe é uma tabela
 @Table(name = "tb_postagens") // indica o nome da tabela de dados
 public class Postagem {
-	
-	
-	@Id  //  ele é a chave primária (PRIMARY KEY)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto incremento
+
+	@Id // ele é a chave primária (PRIMARY KEY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
 	private Long id;
-	
-	@NotBlank(message = "Esse campo é obrigatório")  // Not Null("")
-	@Size(min =5, max = 100, message = "Digite no mínino 5 e no máximo 100 caracteres")
+
+	@NotBlank(message = "Esse campo é obrigatório") // Not Null("")
+	@Size(min = 5, max = 100, message = "Digite no mínino 5 e no máximo 100 caracteres")
 	private String titulo;
-	
-	@NotBlank(message = "Esse campo é obrigatório")  // Not Null("")
+
+	@NotBlank(message = "Esse campo é obrigatório") // Not Null("")
 	@Size(min = 10, max = 1000, message = "Digite no mínino 10 e no máximo 1000 caracteres")
 	private String texto;
-	
+
 	@UpdateTimestamp
-	private LocalDateTime data; // 10/10/25  18:25:30
-	
+	private LocalDateTime data; // 10/10/25 18:25:30
+
+	@ManyToOne
+	@JsonIgnoreProperties("Postagem")
+	private Tema tema;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
